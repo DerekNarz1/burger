c4ed27c1181462cfa859bee30caaf9724904f188
DerekNarz1 <dereknarz1@gmail.com>
2017-09-06 10:40:55 -0700

    Added all Files

---
+++ burger/config/connection.js
@@ -0,0 +1,21 @@
+
+// Node Dependency
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Whatever1', 
+    database : 'burgers_db' 
+  });
+}
+
+
+// Export the Connection
+module.exports = connection;
